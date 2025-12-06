adf8658ca9d122410025de1887d5334ed04e3110
ranjan-alt <ranjankumarsah142@gmail.com>
2025-12-06 22:40:52 +0530

    final

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -7,7 +7,7 @@ import LogoSquare from "@/components/logo-square";
 import CartModal from "@/components/cart/modal";
 
 export async function Navbar() {
-  const menu = await getMenu("next-js-frontend-menu");
+  const menu = await getMenu("customer-account-main-menu");
   return (
     <nav className="flex items-center justify-between p-4 lg:px-6 sticky top-0 backdrop-blur-sm z-[999]">
       <div className="block flex-none md:hidden">

adf439296b21742ac56b29920cddc5c360ca08f1
ranjan-alt <ranjankumarsah142@gmail.com>
2025-12-06 23:40:50 +0530

    css fix

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -14,7 +14,7 @@ export async function Navbar() {
         <MobileMenu menu={menu} />
       </div>
       <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
+        <div className="flex w-full md:w-1/2">
           <Link
             href={"/"}
             prefetch={true}
